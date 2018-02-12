2f2704cf1b5a4116242b086ac9ab1878a59a369e
Nico Schlömer <nico.schloemer@gmail.com>
2018-02-12 14:05:42 +0100

    initial commit

---
+++ tablify/__about__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+__version__ = '0.0.1'
+__author__ = u'Nico Schlömer'
+__author_email__ = 'nico.schloemer@gmail.com'
+__copyright__ = \
+    u'Copyright (c) 2018, {} <{}>'.format(__author__, __author_email__)
+__website__ = 'https://github.com/nschloe/tablify'
+__license__ = 'License :: OSI Approved :: MIT License'
+__status__ = 'Development Status :: 1 - Planning'
---
+++ tablify/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+from __future__ import print_function
+
+from .__about__ import (
+    __version__,
+    __author__,
+    __author_email__,
+    __website__
+    )
+
+# pylint: disable=wildcard-import
+from .main import *
+
+try:
+    import pipdate
+except ImportError:
+    pass
+else:
+    if pipdate.needs_checking(__name__):
+        print(pipdate.check(__name__, __version__))
